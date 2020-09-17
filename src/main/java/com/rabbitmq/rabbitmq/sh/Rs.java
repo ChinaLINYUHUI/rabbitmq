57b4614359d5c18da5d8c220b99c4508a14a915f
1187965150@qq.com <1187965150@qq.com>
2020-09-17 14:53:54 +0800

    commit a file

---
+++ src/main/java/com/rabbitmq/rabbitmq/sh/Rs.java
@@ -0,0 +1,22 @@
+package com.rabbitmq.rabbitmq.sh;
+
+public class Rs {
+    private int id;
+    private String money;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMoney() {
+        return money;
+    }
+
+    public void setMoney(String money) {
+        this.money = money;
+    }
+}
